8d11e988c58a77b287e4408f679ba3e98112d6a9
Aliia Ruslanova <aliyushkaruslanova@gmail.com>
2022-09-26 22:34:32 +0400

    MenuReactProject added to my main repo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # menuReactProject
+https://codesandbox.io/s/reactteamprojectmenu-forked-kj6bhw
---
+++ src/components/RangeSlider.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function RangeSlider(props) {
+  return (
+    <Box sx={{ width: 300 }}>
+      <Slider
+        style={{ color: "var(--clr-gold)" }}
+        getAriaLabel={() => "Temperature range"}
+        value={props.value}
+        onChange={(event) => props.handleChangeSlider(event)}
+        valueLabelDisplay={"auto"}
+        getAriaValueText={valuetext}
+      />
+    </Box>
+  );
+}
